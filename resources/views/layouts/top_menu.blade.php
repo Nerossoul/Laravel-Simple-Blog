6bdeb20b050ab8aa5d39c5ee698f4a83635f65d3
Nerossoul <nerossoul@gmail.com>
2018-06-17 02:26:15 +0700

    Nikita's_blog: change triangle position in sidebar

--- resources/views/layouts/top_menu.blade.php
+++ resources/views/layouts/top_menu.blade.php
@@ -2,7 +2,7 @@
 @if ($category->children->where('published', 1)->count())
 <li class="ananov-dark z-depth-3" onclick="myAccFunc('menu_accordion_{{$category->id}}');event.preventDefault();SendGet('{{$category->slug}}');">
   <a href="{{url(" /blog/category/$category->slug")}}" class="ananov-light-text" style="white-space:nowrap;">
-        {!!$delimiter!!}&nbsp;{{mb_strtoupper($category->title)}}&nbsp;<span class="tiny material-icons" style="font-size:14px;">arrow_drop_down</span>
+        {!!$delimiter!!}&nbsp;<span class="tiny material-icons" style="font-size:14px;">arrow_drop_down</span>&nbsp;{{mb_strtoupper($category->title)}}
       </a>
 </li>
 <div id="menu_accordion_{{$category->id}}" class="ananov-hide ananov-main-color">
